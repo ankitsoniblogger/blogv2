0d9a768bc7a352c8a4b31f0776c530e161b3d96c
Ankit soni <ankitsoniblogger@gmail.com>
2023-05-30 17:08:27 +0400

    bg url

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,12 @@ import BannerText from "./components/BannerText";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import bg from "./assets/bg.jpg";
 
 const App = () => {
   return (
     <Router>
-      <div className="bg-bgImg bg-cover bg-no-repeat h-screen">
+      <div className="bg-[url('./assets/bg.jpg')] bg-cover bg-no-repeat h-screen">
         <Header />
         <Routes>
           <Route path="/" element={<BannerText />} />
